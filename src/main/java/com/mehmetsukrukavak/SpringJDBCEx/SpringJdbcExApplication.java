b076ea8ad987c9cab789a6699f7ca56c6c18fb3f
Mehmet Şükrü Kavak <mehmetsukru.kavak07@gmail.com>
2025-01-08 23:59:14 +0300

    StudentService and StudentRepository

--- src/main/java/com/mehmetsukrukavak/SpringJDBCEx/SpringJdbcExApplication.java
+++ src/main/java/com/mehmetsukrukavak/SpringJDBCEx/SpringJdbcExApplication.java
@@ -1,13 +1,33 @@
 package com.mehmetsukrukavak.SpringJDBCEx;
 
+import com.mehmetsukrukavak.SpringJDBCEx.model.Student;
+import com.mehmetsukrukavak.SpringJDBCEx.service.StudentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringJdbcExApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringJdbcExApplication.class, args);
+
+		ApplicationContext context = SpringApplication.run(SpringJdbcExApplication.class, args);
+
+		Student student = context.getBean(Student.class);
+		student.setRollNo(101);
+		student.setName("John Doe");
+		student.setMarks(95);
+
+		StudentService studentService = context.getBean(StudentService.class);
+
+		studentService.addStudent(student);
+
+		List<Student> students = studentService.getStudents();
+
+		System.out.println(students);
+
 	}
 
 }
---
+++ src/main/java/com/mehmetsukrukavak/SpringJDBCEx/service/StudentService.java
@@ -0,0 +1,32 @@
+package com.mehmetsukrukavak.SpringJDBCEx.service;
+
+import com.mehmetsukrukavak.SpringJDBCEx.model.Student;
+import com.mehmetsukrukavak.SpringJDBCEx.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+
+    private StudentRepository studentRepository;
+
+    public StudentRepository getStudentRepository() {
+        return studentRepository;
+    }
+
+    @Autowired
+    public void setStudentRepository(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public void addStudent(Student student) {
+        studentRepository.save(student);
+    }
+
+    public List<Student> getStudents() {
+        return studentRepository.findAll();
+    }
+}

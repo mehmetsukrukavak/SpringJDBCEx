01df7408ced275cf1edbf639fd509f4062d6f6c7
Mehmet Şükrü Kavak <mehmetsukru.kavak07@gmail.com>
2025-01-09 18:56:07 +0300

    data.sql, schema.sql, row mapper

--- src/main/java/com/mehmetsukrukavak/SpringJDBCEx/repository/StudentRepository.java
+++ src/main/java/com/mehmetsukrukavak/SpringJDBCEx/repository/StudentRepository.java
@@ -1,8 +1,14 @@
 package com.mehmetsukrukavak.SpringJDBCEx.repository;
 
 import com.mehmetsukrukavak.SpringJDBCEx.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,12 +16,40 @@ import java.util.List;
 public class StudentRepository {
 
 
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    @Autowired
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     public void save(Student student) {
-        System.out.println("Added Student");
+        String sql = "INSERT INTO student (rollNo, name, marks) VALUES (?, ?, ?)";
+
+       int rows = jdbcTemplate.update(sql, student.getRollNo(), student.getName(), student.getMarks());
+        System.out.println(rows + " rows inserted");
     }
 
     public List<Student> findAll() {
-        List<Student> students = new ArrayList<>();
-        return students;
+        String sql = "SELECT * FROM student";
+        RowMapper<Student> rowMapper = new RowMapper<Student>() {
+            @Override
+            public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+                Student student = new Student();
+                student.setRollNo(rs.getInt("rollNo"));
+                student.setName(rs.getString("name"));
+                student.setMarks(rs.getInt("marks"));
+
+                return student;
+            }
+        };
+
+        return jdbcTemplate.query(sql, rowMapper);
+
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into student(rollNo, name, marks) values (101,'Esra', 100);
+insert into student(rollNo, name, marks) values (102,'Junior', 95);
+insert into student(rollNo, name, marks) values (103,'Kavak', 75);
+
